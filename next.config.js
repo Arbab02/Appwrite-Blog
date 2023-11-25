740ef6404730351fd164a09c4c9c0ef53e608beb
Arbab Raheem <121390455+Arbab02@users.noreply.github.com>
2023-11-25 10:04:43 -0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: "export",
+};
 
-module.exports = {
-    images: {
-        domains: ['images.unsplash.com'],
-    },
-}
+module.exports = nextConfig
